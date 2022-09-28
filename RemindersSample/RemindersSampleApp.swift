39799fe096e454dd6a2fd7b9533177f15f02ba92
Ryo <21cm0108@jec.ac.jp>
2022-09-28 09:13:44 +0900

    Initial Commit

---
+++ RemindersSample/RemindersSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindersSampleApp.swift
+//  RemindersSample
+//
+//  Created by Ryo on 2022/09/28.
+//
+
+import SwiftUI
+
+@main
+struct RemindersSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
